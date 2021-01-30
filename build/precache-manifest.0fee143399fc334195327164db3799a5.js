63aa5abdce213762215f2e8f95bc8a74e3acf720
codarstartup <codarstartup@gmail.com>
2021-01-30 19:36:35 -0300

    build

---
+++ build/precache-manifest.0fee143399fc334195327164db3799a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee678364834ce51657bada740e127113",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7fc477c325f795046eb0",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "e7006c29ad561a8c285a",
+    "url": "/static/js/2.8ec8fc10.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8ec8fc10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7fc477c325f795046eb0",
+    "url": "/static/js/main.7f3d6db2.chunk.js"
+  },
+  {
+    "revision": "d98d8112ca80e5704569",
+    "url": "/static/js/runtime-main.135d0715.js"
+  }
+]);
